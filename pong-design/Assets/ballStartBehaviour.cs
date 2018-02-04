ae6597d47e5302f95f654d85f2060d3e296c2f7f
qiqi0725 <yu.qi.liu236@gmail.com>
2018-02-03 21:07:56 -0500

    ???

--- pong-design/Assets/ballStartBehaviour.cs
+++ pong-design/Assets/ballStartBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ballStartBehaviour : MonoBehaviour {
 
@@ -8,18 +9,19 @@ public class ballStartBehaviour : MonoBehaviour {
     int Invert;
     public int TowardsPlayer = 1;
     private Rigidbody myRigidbody;
+    public float speed = 30;
 
     // Use this for initialization
     void Start () {
         Invert = 1;
         TowardsPlayer = 1;
         this.myRigidbody = this.GetComponent<Rigidbody>();
-        myRigidbody.velocity = new Vector3(10.0f, 0.0f, 5.0f);
+        myRigidbody.velocity = new Vector3(10.0f, 0.0f, 15.0f);
     }
 	
 	// Update is called once per frame
 	void Update () {
-
+        /*
         if (transform.position.z < -5.5f)
         {
 
@@ -38,30 +40,33 @@ public class ballStartBehaviour : MonoBehaviour {
             transform.position = Vector3.zero;
 
         }
-
+        */
     }
+
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name == "wall")
+       
+        if (collision.collider.tag == "RacketLeft")
         {
             Invert = Invert * -1;
-
-            myRigidbody.velocity = new Vector3(10.0f * Invert, 0.0f, -5.0f * TowardsPlayer);
+            myRigidbody.velocity = new Vector3(10.0f * Invert, 0.0f, -15.0f * TowardsPlayer); 
 
         }
-        else if (collision.gameObject.name == "player")
+        else if (collision.collider.tag == "RacketRight")
         {
-
             TowardsPlayer = 1;
-            myRigidbody.velocity = new Vector3(10.0f * Invert, 0.0f, -5.0f);
-
+            myRigidbody.velocity = new Vector3(10.0f * Invert, 0.0f, 15.0f);
+            Debug.Log("touched right racket");
         }
-        else if (collision.gameObject.name == "enemy")
+        else if (collision.collider.tag == "WallLeft")
         {
-
-            TowardsPlayer = -1;
-            myRigidbody.velocity = new Vector3(10.0f * Invert, 0.0f, 5.0f);
-
+            Application.Quit();
+            Debug.Log("right player wins");
+        }
+        else if (collision.collider.tag == "WallRight")
+        {
+            Application.Quit();
+            Debug.Log("left player wins");
         }
     }
 }
