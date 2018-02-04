645e4c636e3c7991ce5a8449d793e5dfd409e812
Federico Naranjo <federico.naranjo@gmail.com>
2018-02-03 21:40:28 -0500

    start scene menu

---
+++ pong-design/Assets/ButtonManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+	public void NewSinglePlayerGame(string newSinglePlayerGame) {
+		SceneManager.LoadScene(newSinglePlayerGame);
+	}
+	
+	// Update is called once per frame
+	public void Update() {
+		
+	}
+}
