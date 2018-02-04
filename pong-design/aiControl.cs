d182d740797dd20995c5ab1766eae07ceb40fff7
qiqi0725 <30781074+qiqi0725@users.noreply.github.com>
2018-02-04 03:53:50 -0500

    controls for different scene
    refer to hierarchy file to link them properly

---
+++ pong-design/aiControl.cs
@@ -0,0 +1,30 @@
+﻿// control the paddle using "ai"
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class racketRightPlayer : MonoBehaviour {
+
+    public GameObject sphere;
+
+    // Use this for initialization
+    void Start () {
+        sphere = GameObject.Find("Ball"); 
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if (sphere.transform.position.x > transform.position.x && if transform.position.x < 7.5)
+        {
+
+            transform.position = new Vector3(transform.position.x + 0.2f, transform.position.y, transform.position.z);
+            
+        }
+        if (sphere.transform.position.x < transform.position.x && && if transform.position.x > -7.5)
+        {
+            
+            transform.position = new Vector3(transform.position.x - 0.2f, transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ pong-design/keyboardControl.cs
@@ -0,0 +1,30 @@
+﻿// control the paddle using "up" and "down" arrow keys
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class racketLeftPlayer : MonoBehaviour {
+
+	 
+    // Update is called once per frame
+    void Update()
+    {
+     
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+
+        if (Input.GetKey("up") && transform.position.x < 7.5)
+        {
+
+            transform.position = new Vector3(transform.position.x + 0.2f, 0.0f, transform.position.z);
+            Debug.Log(transform.position.x);
+        }
+        if (Input.GetKey("down") && transform.position.x > -7.5)
+        {
+            transform.position = new Vector3(transform.position.x - 0.2f, 0.0f, transform.position.z);
+            Debug.Log(transform.position.x);
+        }
+
+    }
+}
